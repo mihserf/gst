b273b1ae7c6e03c37a0c5476fc5174ac790733aa
Michail <gaganov@gmail.com>
2008-06-20 11:23:54 +0300

    status_name not required now

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,10 +2,10 @@ class Status < ActiveRecord::Base
   has_many :member_statuses, :dependent => :destroy
   has_many :members, :through => :member_statuses, :dependent => :destroy
   has_many :translations, :class_name => 'StatusTranslation', :dependent => :destroy
-  
+
   translate_columns :name, :status_name
 
-  validates_presence_of :name, :status_name
+  validates_presence_of :name
 
   include SetIdentName
 
