f3a46378ecaf0dc12ad9540ab9429b448ce594e4
Michail <gaganov@gmail.com>
2008-10-30 14:49:20 +0200

    events fixes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.xml
   def index
-    @events = Event.find(:all, :select => "title,ident_name,short_text", :order => "created_at DESC")
+    @events = Event.find(:all, :select => "id,title,ident_name,short_text", :order => "created_at DESC")
     
     respond_to do |format|
       format.html { render :template => "events/list" unless admin?}
