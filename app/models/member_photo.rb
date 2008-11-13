a874280006eaeac42c32f5ee174c4913aaab692b
Michail <gaganov@gmail.com>
2008-11-13 16:29:38 +0200

    member_photo resize changed

--- app/models/member_photo.rb
+++ app/models/member_photo.rb
@@ -1,7 +1,7 @@
 class MemberPhoto < ActiveRecord::Base
   belongs_to :member
 
-  has_attachment  :max_size => 2.megabytes, :content_type => :image, :resize_to => '420x300>',   :thumbnails => { :thumb => 'x50', :view => 'x120' },
+  has_attachment  :max_size => 2.megabytes, :content_type => :image, :resize_to => 'x300',   :thumbnails => { :thumb => 'x50', :view => 'x120' },
     :storage => :file_system, :path_prefix => 'public/photos/members', :processor => 'ImageScience'
   validates_as_attachment
 
